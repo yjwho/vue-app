06bbda33c040f58f5d38b7a3db9b2952029c65b0
yjwho <hyt201291@gmail.com>
2019-02-19 17:46:06 -0700

    more styling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/issues">Issues</router-link> |
+    
+      <ul>
+        <li>
+          <router-link to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/issues">Issues</router-link>
+        </li>
+        <li>
             <router-link to="/about">About</router-link>
-    </div>
+        </li>
+      </ul>
+  
     <router-view />
   </div>
 </template>
@@ -21,12 +29,81 @@
   padding: 30px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+
+
+h1, h2 {
+  display: inline-block;
+  color: #FFFFFF
+}
+h1 {
+  font-size: 80px;
+  font-family: 'Rubik Mono One', sans-serif;
+}
+h2 {
+  font-size: 20px;
+  font-family: 'Merriweather', sans-serif;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+/*Menu Bar Styling*/
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+/*  border-radius: 25px; */
 }
+
+li {
+  float: left;
+  font-size: 20px;
+  font-family: 'Lato', sans-serif;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+  background-color: #000780;
+  color: white;
+}
+
+a.router-link-exact-active {
+  background-color: #ABE5E6;
+  color: darkslategrey;
+  font-weight: bold;
+}
+
+/* Button Styling */
+.button {
+  font-family: 'Lato', sans-serif;
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 25px;
+  margin: 4px 2px;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+.button1 {
+  background-color: #F05E80; 
+  border: 2px solid #F05E80;
+}
+
+.button1:hover {
+  background-color: white;
+  color: #F05E80;
+}
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,87 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
+      A paragraph here.
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
   </div>
 </template>
 
@@ -103,81 +24,12 @@ html {
 }
 body {
   text-align: center;
-  background: linear-gradient(to bottom right, #fccdd3, #c9a8bc, #9386a0, #5e677e, #2f4858);
+  
+  background: linear-gradient(45deg,#ABE5E6,#7062F0);
+  /*linear-gradient(to bottom right, #fccdd3, #c9a8bc, #9386a0, #5e677e, #2f4858);*/
   background-size: cover;
 }
 
-h1, h2 {
-  display: inline-block;
-  color: #FFFFFF
-}
-h1 {
-  font-size: 80px;
-  font-family: 'Rubik Mono One', sans-serif;
-}
-h2 {
-  font-size: 20px;
-  font-family: 'Merriweather', sans-serif;
-}
-
-/*Menu Bar Styling*/
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  border-radius: 25px;
-}
-
-li {
-  float: left;
-  font-size: 20px;
-  font-family: 'Lato', sans-serif;
-}
-
-li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-/* Change the link color to #111 (black) on hover */
-li a:hover {
-  background-color: #9A89B4;
-}
-
-.active {
-  background-color: #4A4453;
-}
-
-/* Button Styling */
-.button {
-  font-family: 'Lato', sans-serif;
-  background-color: #4CAF50; /* Green */
-  border: none;
-  color: white;
-  padding: 16px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 25px;
-  margin: 4px 2px;
-  -webkit-transition-duration: 0.4s; /* Safari */
-  transition-duration: 0.4s;
-  cursor: pointer;
-}
-
-.button1 {
-  background-color: #F05E80; 
-  border: 2px solid #F05E80;
-}
-
-.button1:hover {
-  background-color: white;
-  color: #F05E80;
-}
 
 
 
