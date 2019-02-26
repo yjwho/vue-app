7ede67a7b6cb14a80db113be559a6ebe71c28f6a
yjwho <hyt201291@gmail.com>
2019-02-26 15:35:26 -0700

    upvotes and comments are here

---
+++ src/components/Post.vue
@@ -0,0 +1,94 @@
+<template>
+    <div v-if="post !== null" class="section post">
+        <article :class="{media: true, hot: isHot}">
+
+            <figure class="media-left">
+                <img :src="post.submissionImage" alt="submission image" class="image is-64x64">
+            </figure>
+
+            <div class="media-content">
+                <div class="content">
+                    <p>
+                        <strong>
+                            <a href="#" class="has-text-info">{{ post.title }}</a>
+                            <span class="tag is-small">#{{ post.id }}</span>
+                        </strong>
+                        <br>
+                            {{ post.content }}
+                        <br>
+                        <small class="is-size-7">
+                            
+                        </small>
+                    </p>
+                </div>
+            </div>
+
+            <div class="media-right">
+                <span class="icon is-small">
+                    <i @click="increaseVotes()" class="fa fa-chevron-up"></i>
+                    <strong class="has-text-info">{{ post.votes }}</strong>
+                </span>
+                <button @click="increaseVotes()" class="button-primary">Upvote {{ post.votes }}</button>
+            </div>
+
+        </article>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        post: null
+    },
+    methods: {
+        increaseVotes: function () {
+            this.post.votes++;
+        }
+    },
+    computed: {
+        isHot: function () {
+            return this.post.votes >= 20;
+        }
+    }
+}
+</script>
+
+
+// STYLE
+<style lang='scss' scoped>
+
+.post p {
+    font-size: calc(12px + 0.5vw);
+}
+
+.section {
+    padding: 0.5rem 0.5rem;
+}
+.media {
+    max-width: 1000px;
+    margin: 0 auto;
+    border: 1px solid #e6e7e9;
+    padding: 1em 1.5em 0.5em 1.5em;
+    border-radius: 0.3em;
+}
+
+// Once you reach 20 votes.
+@mixin border-gradient($from, $to, $weight: 0) {
+    $mix-main: mix($from, $to);
+    $mix-sub-from: mix($mix-main, $from);
+    $mix-sub-to: mix($mix-main, $to);
+
+    box-shadow: 0 1px 0 $weight rgba($mix-sub-to, .25),
+                0 -1px 0 $weight rgba($mix-sub-from, .25),
+                1px 0 0 $weight rgba($mix-sub-to, .25),
+                -1px 0 0 $weight  rgba($mix-sub-from, .25),
+                1px -1px 0 $weight rgba($mix-main, .5),
+                -1px 1px 0 $weight rgba($mix-main, .5),
+                1px 1px 0 $weight rgba($to, .75),
+                -1px -1px 0 $weight rgba($from, .75);
+}
+.hot {
+    @include border-gradient(rgb(255, 72, 22), rgb(255, 185, 0));
+}
+
+</style>
--- src/views/Issues.vue
+++ src/views/Issues.vue
@@ -1,17 +1,24 @@
 <template>
+<!--
 <div class="issues">
 <Issue name = "Jackie" issue="I called Alex white."/>
 <Issue name = "Jackie" issue="Alex is white."/>
 </div>
+ -->
+<div id="app" class="is-marginless is-paddingledd">
+    <UpVote></UpVote>
+</div>
 </template>
 
 <script>
-import Issue from "@/components/story.vue";
+//import Issue from "@/components/story.vue";
+import UpVote from "@/components/UpVote";
 
 export default {
-name: "issues",
-components: {
-Issue
-}
+    name: "issues",
+    components: {
+        //Issue
+        UpVote
+    }
 };
 </script>
